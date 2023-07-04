0aa7335e719162bb0db32c61da3bd446b91ffb8b
chamalferdy <chamal404fido@gmail.com>
2023-07-04 08:15:49 +0530

    second commit message

--- Labsheet_04/q01.scala
+++ Labsheet_04/q01.scala
@@ -7,9 +7,9 @@ def main(args: Array[String]): Unit = {
 }
 
 def interestfunc(deposit: Double): Double = deposit match {
-  case x if x < 0       => 0
-  case x if x < 20000   => x * 0.02
-  case x if x < 200000  => x * 0.04
-  case x if x < 2000000 => x * 0.035
-  case x if x > 2000000 => x * 0.065
+  case x if x < 0         => 0
+  case x if x < 20_000    => x * 0.02
+  case x if x < 200_000   => x * 0.04
+  case x if x < 2_000_000 => x * 0.035
+  case x if x > 2_000_000 => x * 0.065
 }

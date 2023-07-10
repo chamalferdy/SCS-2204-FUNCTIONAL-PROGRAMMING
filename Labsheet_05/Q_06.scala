9237d5b4ac5aa7e17f45256fcb7b987915091174
chamalferdy <chamal404fido@gmail.com>
2023-07-10 21:59:55 +0530

    commit message for recorrection

--- Labsheet_05/Q_06.scala
+++ Labsheet_05/Q_06.scala
@@ -1,24 +1,27 @@
 /*The Fibonacci Sequence is the series of numbers: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, . . .
 Each subsequent number is the sum of the previous two. Write a recursive function print fist n Fibonacci numbers for given n.*/
-
-def main(args: Array[String]): Unit = {
+object Fibonacci{
+  def main(args: Array[String]): Unit = {
     var flag : Boolean = true
     while (flag) {
-        println("Enter a number : ")
-        var m: Int = scala.io.StdIn.readInt()
-        if m >= 1 then println(fibonacci(m)) 
-        else{
-            println("Enter a Natural Number !")
-            flag = false
-        }
+      println("\nEnter a number : ")
+      var m: Int = scala.io.StdIn.readInt()
+      if m >= 1 then printFibonacci(m)
+      else{
+        println("\nEnter a Natural Number !")
+        flag = false
+      }
     }
-}
-
-def fibonacci(f: Int): Int = {
-  if f == 1 then 0
-  else if f == 2 then 1
-  else{
-    var value : Int = fibonacci(f - 1) + fibonacci(f - 2)
-    value
+  }
+  def printFibonacci(n: Int): Unit = {
+    for (i <- 0 until n) {
+      print(fibonacci(i) + " ")
+    }
+  } 
+  def fibonacci(n: Int): Int = {
+    if (n <= 1)
+      n
+    else
+      fibonacci(n - 1) + fibonacci(n - 2)
   }
 }

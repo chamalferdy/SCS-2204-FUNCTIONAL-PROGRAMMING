220c5cbab9fdfe79c1aa618d12543cebd5e03837
chamalferdy <chamal404fido@gmail.com>
2023-08-07 20:20:59 +0530

    commit message

--- Labsheet_07/Q03.scala
+++ Labsheet_07/Q03.scala
@@ -10,8 +10,6 @@ object Three {
   def filterPrime(numbers: List[Int]): List[Int] = {
     val isPrime = (n: Int) =>
       (n > 1) && (2 to Math.sqrt(n).toInt).forall(n % _ != 0)
-      // (2 to Math.sqrt(n).toInt).forall(n % _ != 0) checks if none of the numbers from 2 to the square root of n evenly divide n.
-      // The _ is a placeholder that represents the current value in the range. n % _ != 0 checks if n is not divisible by the current value.
     numbers.filter(isPrime)
   }
 }
